200539b1c17c4af7503be840dadb0e7bf4cc7c0d
olivianoell <oliviacatherinenoell@gmail.com>
2025-02-13 18:45:11 -0500

    html structure, component added, page added

---
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -0,0 +1,13 @@
+import QuestionCard from "../../components/QuestionCard/QuestionCard";
+
+const MyQuestionPage = () => {
+
+  return (
+    <>
+        <QuestionCard />
+        <button>Next Question</button>
+    </>
+  );
+};
+
+export default MyQuestionPage;
