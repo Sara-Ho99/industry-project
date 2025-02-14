19f8f87e82ec01cee2d05b7ea66868a52fdc5bd2
olivianoell <oliviacatherinenoell@gmail.com>
2025-02-13 20:50:58 -0500

    data fetching from api into QuestionPage and QuestionCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import ModulesPage from "./pages/modulesPage/modulesPage";
+import QuestionPage from "./pages/QuestionPage/QuestionPage";
 
 function App() {
   return (
     <>
-      <h1>This is a quiz app for AI basics</h1>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<ModulesPage />} /> */}
+        <Route path="/easy/:questionId" element={<QuestionPage />} />
+        <Route
+          path="/intermediate/:questionId"
+          element={<Navigate to="/easy/:questionId" />}
+        />
+        <Route
+          path="/advanced/:questionId"
+          element={<Navigate to="/easy/:questionId" />}
+        />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,23 +1,31 @@
 import "./QuestionCard.scss";
+import React, { useState, useEffect } from "react";
 
-const QuestionCard = () => {
+const QuestionCard = ({ questionData }) => {
+  const { question, correct_answer, incorrect_answers, explanation, link } = questionData;
+  const options = [correct_answer, ...incorrect_answers];
 
-    return (
-        <section>
-            <h2>Question</h2>
-            <ul>
-                <li>Option 1</li>
-                <li>Option 2</li>
-                <li>Option 3</li>
-                <li>Option 4</li>
-            </ul>
-            <div>
-                <h3>Explanation</h3>
-                <p>explanation...</p>
-                <p>Additonal Resources</p>
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <h2>{question}</h2>
+      <ul>
+        {options.map((option, index) => (
+          <li key={index}>
+            {option}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <h3>Explanation</h3>
+        <p>{explanation}</p>
+        <p>
+          <a href={link}>
+            Additional Resources
+          </a>
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default QuestionCard;
